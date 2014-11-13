cf2c802cd9d77ba0e036577f501dda13650ac2da
allyraza <alirz23@gmail.com>
2014-11-13 16:26:00 +0200

    update vendor path

--- protected/config/main.php
+++ protected/config/main.php
@@ -51,7 +51,7 @@ return array(
 		
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=test',
+			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
